8c5a3e27fc2e83440ab3f7bf299d1fb21c901f8b
SachinSulkunte <41236722+SachinSulkunte@users.noreply.github.com>
2024-02-09 11:00:17 -0500

    new test for 2-axis movement

--- apriltag.py
+++ apriltag.py
@@ -45,6 +45,8 @@ if __name__ == '__main__':
     ep_camera = ep_robot.camera
     ep_camera.start_video_stream(display=False, resolution=camera.STREAM_360P)
 
+    ep_chassis = ep_robot.chassis
+
     tag_size=0.16 # tag size in meters
 
     while True:
@@ -58,113 +60,62 @@ if __name__ == '__main__':
 
             results = at_detector.detect(gray, estimate_tag_pose=False)
             for res in results:
-                # print("result:", res)
+                print(res)
                 pose = find_pose_from_tag(K, res)
                 rot, jaco = cv2.Rodrigues(pose[1], pose[1])
-                # print(rot)
+                print(rot)
 
                 pts = res.corners.reshape((-1, 1, 2)).astype(np.int32)
                 img = cv2.polylines(img, [pts], isClosed=True, color=(0, 0, 255), thickness=5)
                 cv2.circle(img, tuple(res.center.astype(np.int32)), 5, (0, 0, 255), -1)
-                print(res.center.astype(np.int32)[0])
-                # print(pose)
-                
-             
-                ep_chassis = ep_robot.chassis
 
-                x_val = res.center.astype(np.int32)[0]
-                y_val = res.center.astype(np.int32)[1]
-                z_val = 90
+                print(pose)
 
-                # if(res.center.astype(np.int32)[0]<110):
-                #     # Forward 0.5 meters
-                #     ep_chassis.move(x=0.5, y=0, z=0, xy_speed=0.7).wait_for_completed()
-
-                #     # Backward 0.5 meters
-                #     ep_chassis.move(x=-0.5, y=0, z=0, xy_speed=0.7).wait_for_completed()
-
-                #     # Move 0.5 meters left
-                #     ep_chassis.move(x=0, y=-0.5, z=0, xy_speed=0.7).wait_for_completed()
-
-                #     # Move 0.5 meters right
-                #     ep_chassis.move(x=0, y=0.5, z=0, xy_speed=0.7).wait_for_completed()
-
-                #     # Trun left 90 degrees
-                #     ep_chassis.move(x=0, y=0, z=0.5, z_speed=45).wait_for_completed()
-
-                #     # Trun right 90 degrees
-                #     ep_chassis.move(x=0, y=0, z=-90, z_speed=45).wait_for_completed()
-
-                #     ep_robot.close()
                 curr_x = res.center.astype(np.int32)[0]
                 curr_y = res.center.astype(np.int32)[1]
 
                 goal_x = 320
-                goal_y = 200
+                goal_y = 110
 
                 x_val = 0.3
                 y_val = 0.3
-                z_val = 90
                 
-                if (curr_x < goal_x):
-                    ep_chassis.drive_speed(x=0, y=-y_val, z=0, timeout=1)
-                    time.sleep(1)
-                if (curr_x > goal_x):
-                    ep_chassis.drive_speed(x=0, y=y_val, z=0, timeout=1)
-                    time.sleep(1)
-                if (curr_x == goal_x):
-                    time.sleep(1)
-                if (curr_y < goal_y):
-                    ep_chassis.drive_speed(x=x_val, y=0, z=0, timeout=1)
-                    time.sleep(1)
-                if (curr_y > goal_y):
-                    ep_chassis.drive_speed(x=-x_val, y=0, z=0, timeout=1)
-                    time.sleep(1)
-                if (curr_y == goal_y):
-                    time.sleep(1)
+                # if (curr_x < goal_x):
+                #     ep_chassis.drive_speed(x=0, y=y_val, z=0, timeout=1)
+                #     time.sleep(0.5)
+                # if (curr_x > goal_x):
+                #     ep_chassis.drive_speed(x=0, y=-y_val, z=0, timeout=1)
+                #     time.sleep(0.5)
+                # if (curr_x == goal_x):
+                #     time.sleep(0.5)
 
+                # if (curr_y < goal_y):
+                #     ep_chassis.drive_speed(x=-x_val, y=0, z=0, timeout=1)
+                #     time.sleep(0.5)
+                # if (curr_y > goal_y):
+                #     ep_chassis.drive_speed(x=x_val, y=0, z=0, timeout=1)
+                #     time.sleep(0.5)
+                # if (curr_y == goal_y):
+                #     time.sleep(0.5)
+
+                if (curr_x < goal_x and curr_y < goal_y):
+                    ep_chassis.drive_speed(x=-x_val, y=y_val, z=0, timeout=1)
+                    time.sleep(0.5)
+                elif (curr_x < goal_x and curr_y > goal_y):
+                    ep_chassis.drive_speed(x=x_val, y=y_val, z=0, timeout=1)
+                    time.sleep(0.5)
+                elif (curr_x > goal_x and curr_y < goal_y):
+                    ep_chassis.drive_speed(x=-x_val, y=-y_val, z=0, timeout=1)
+                    time.sleep(0.5)
+                elif (curr_x > goal_x and curr_y > goal_y):
+                    ep_chassis.drive_speed(x=x_val, y=-y_val, z=0, timeout=1)
+                    time.sleep(0.5)
 
             cv2.imshow("img", img)
             cv2.waitKey(10)
-            
-            
-            ## time.sleep(3)
+
         except KeyboardInterrupt:
             ep_camera.stop_video_stream()
             ep_robot.close()
             print ('Exiting')
             exit(1)
-
-
-
-# if __name__ == '__main__':
-#     ep_robot = robot.Robot()
-#     ep_robot.initialize(conn_type="ap")
-
-#     ep_chassis = ep_robot.chassis
-
-#     x_val = res.center.astype(np.int32)[0]
-#     y_val = res.center.astype(np.int32)[1]
-#     z_val = 90
-
-#     # Forward 0.5 meters
-#     ep_chassis.move(x=x_val, y=0 z=0, xy_speed=0.7).wait_for_completed()
-
-#     # Backward 0.5 meters
-#     ep_chassis.move(x=-x_val, y=0, z=0, xy_speed=0.7).wait_for_completed()
-
-#     # Move 0.5 meters left
-#     ep_chassis.move(x=0, y=-y_val, z=0, xy_speed=0.7).wait_for_completed()
-
-#     # Move 0.5 meters right
-#     ep_chassis.move(x=0, y=y_val, z=0, xy_speed=0.7).wait_for_completed()
-
-#     # Trun left 90 degrees
-#     ep_chassis.move(x=0, y=0, z=z_val, z_speed=45).wait_for_completed()
-
-#     # Trun right 90 degrees
-#     ep_chassis.move(x=0, y=0, z=-z_val, z_speed=45).wait_for_completed()
-
-#     ep_robot.close()
-
-
