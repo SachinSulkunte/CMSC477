8bdea65b66d646b8601d6a9b48230549f451f5f5
SachinSulkunte <41236722+SachinSulkunte@users.noreply.github.com>
2024-03-08 13:48:33 -0500

    Update test_aptag_move.py

--- Lab 1/test_aptag_move.py
+++ Lab 1/test_aptag_move.py
@@ -44,6 +44,15 @@ def change_direction(stage, path, current_ind):
     x = path[current_ind].split(' ')[0]
     y = path[current_ind].split(' ')[1]
 
+    next_x = path[current_ind + 1].split(' ')[0]
+    next_y = path[current_ind + 1].split(' ')[1]
+
+    if x < next_x:
+        y_vel = 0.3
+        x_vel = 0
+    else:
+        y_vel = -0.3
+        x_vel = 0
     
     return stage
 
@@ -54,14 +63,12 @@ if __name__ == '__main__':
     ep_robot = robot.Robot()
     ep_robot.initialize(conn_type="ap")
     ep_camera = ep_robot.camera
-    ep_camera.start_video_stream(display=False, resolution=camera.STREAM_360P)
+    ep_camera.start_video_stream(display=True, resolution=camera.STREAM_360P)
+    ep_chassis = ep_robot.chassis
 
     path = aprilTagPathing.compute_path()
 
     tag_size=0.16 # tag size in meters
-
-    ep_chassis = ep_robot.chassis
-
 
     x_val = 0.0
     y_val = 0.3
@@ -81,6 +88,7 @@ if __name__ == '__main__':
             results = at_detector.detect(gray, estimate_tag_pose=False)
             for res in results:
                 if res.tag_id == 38 and stage == 1:
+                    print("see 38")
                     pose = find_pose_from_tag(K, res)
                     rot, jaco = cv2.Rodrigues(pose[1], pose[1])
 
@@ -94,14 +102,49 @@ if __name__ == '__main__':
                     
                     goal_y = -0.05
                     goal_x = 0.2
-                    duration = 0.5
+                    duration = 0.75
         
                     vel_x = (curr_x - goal_x) / duration
                     vel_y = (curr_y - goal_y) / duration
 
-                    ep_chassis.drive_speed(x=vel_x, y=vel_y, z=rot, timeout=duration)
+                    ep_chassis.drive_speed(x=vel_x, y=vel_y, z=0, timeout=duration)
+
+                    if abs(curr_y - goal_y) < 0.05 and abs(curr_x -  goal_x) < 0.05:
+                        print("moving next stage")
+                        stage = 2
+                        ep_chassis.drive_speed(x=x_val, y=-y_val, z=z_val, timeout=5)
                 
-                elif res.tag_id == 44:
+                elif res.tag_id == 44 and stage == 2:
+                    pose = find_pose_from_tag(K, res)
+                    rot, jaco = cv2.Rodrigues(pose[1], pose[1])
+
+                    pts = res.corners.reshape((-1, 1, 2)).astype(np.int32)
+                    img = cv2.polylines(img, [pts], isClosed=True, color=(0, 0, 255), thickness=5)
+                    # cv2.circle(img, tuple(res.center.astype(np.int32)), 5, (0, 0, 255), -1)
+
+                    curr_y = round(pose[0][0],2)
+                    curr_x = round(pose[0][2], 2)
+                    rot = round(pose[1][2], 2) * 1000 / 3.14
+                    
+                    goal_y = 0.05
+                    goal_x = 0.2
+                    duration = 0.75
+        
+                    vel_x = (curr_x - goal_x) / duration
+                    vel_y = (curr_y - goal_y) / duration
+
+                    ep_chassis.drive_speed(x=vel_x, y=vel_y, z=0, timeout=duration)
+
+                    if abs(curr_y - goal_y) < 0.05 and abs(curr_x -  goal_x) < 0.05:
+                        stage = 3
+                        # print("start spin")
+                        # ep_chassis.move(x=0, y=0, z=-180, z_speed=1).wait_for_completed()
+                        # print("spinning")
+                        ep_chassis.drive_speed(x=x_val, y=-y_val, z=30, timeout=5)
+                        time.sleep(6)
+                        ep_chassis.drive_speed(x=0, y=-y_val, z=0, timeout=5)
+
+                elif res.tag_id == 39 and stage == 3:
                     pose = find_pose_from_tag(K, res)
                     rot, jaco = cv2.Rodrigues(pose[1], pose[1])
 
@@ -120,8 +163,11 @@ if __name__ == '__main__':
                     vel_x = (curr_x - goal_x) / duration
                     vel_y = (curr_y - goal_y) / duration
 
-                    ep_chassis.drive_speed(x=vel_x, y=vel_y, z=rot, timeout=duration)
+                    ep_chassis.drive_speed(x=vel_x, y=vel_y, z=0, timeout=duration)
 
+                    if abs(curr_y - goal_y) < 0.05 and abs(curr_x -  goal_x) < 0.05:
+                        stage = 4
+                        ep_chassis.drive_speed(x=0.3, y=0, z=0, timeout=5)
             cv2.imshow("img", img)
             cv2.waitKey(6)
 
