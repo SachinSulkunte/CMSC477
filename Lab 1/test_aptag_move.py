0e00f6aa84e3d10110645d06eae29212247d0b24
SachinSulkunte <41236722+SachinSulkunte@users.noreply.github.com>
2024-03-08 12:22:45 -0500

    Update test_aptag_move.py

--- Lab 1/test_aptag_move.py
+++ Lab 1/test_aptag_move.py
@@ -4,6 +4,7 @@ import numpy as np
 import time
 from robomaster import robot
 from robomaster import camera
+import aprilTagPathing
 
 at_detector = Detector(
     families="tag36h11",
@@ -38,6 +39,13 @@ def find_pose_from_tag(K, detection):
 
     return p.reshape((3,)), r.reshape((3,))
 
+def change_direction(stage, path, current_ind):
+
+    x = path[current_ind].split(' ')[0]
+    y = path[current_ind].split(' ')[1]
+
+    
+    return stage
 
 if __name__ == '__main__':
     ep_robot = robot.Robot()
@@ -48,6 +56,8 @@ if __name__ == '__main__':
     ep_camera = ep_robot.camera
     ep_camera.start_video_stream(display=False, resolution=camera.STREAM_360P)
 
+    path = aprilTagPathing.compute_path()
+
     tag_size=0.16 # tag size in meters
 
     ep_chassis = ep_robot.chassis
@@ -57,6 +67,8 @@ if __name__ == '__main__':
     y_val = 0.3
     z_val = 0
     
+    stage = 1 # tracking coordinate step through map
+
     ep_chassis.drive_speed(x=x_val, y=y_val, z=z_val, timeout=5)
     while(1):
         try:
@@ -66,26 +78,49 @@ if __name__ == '__main__':
             gray.astype(np.uint8)
 
             K=np.array([[184.752, 0, 320], [0, 184.752, 180], [0, 0, 1]])
-
             results = at_detector.detect(gray, estimate_tag_pose=False)
             for res in results:
-                if res.tag_id == 38:
-                    ep_chassis.drive_speed(x=0, y=0, z=0, timeout=5)
-                print(res)
-                pose = find_pose_from_tag(K, res)
-                rot, jaco = cv2.Rodrigues(pose[1], pose[1])
-                print(rot)
+                if res.tag_id == 38 and stage == 1:
+                    pose = find_pose_from_tag(K, res)
+                    rot, jaco = cv2.Rodrigues(pose[1], pose[1])
 
-                pts = res.corners.reshape((-1, 1, 2)).astype(np.int32)
-                img = cv2.polylines(img, [pts], isClosed=True, color=(0, 0, 255), thickness=5)
-                cv2.circle(img, tuple(res.center.astype(np.int32)), 5, (0, 0, 255), -1)
+                    pts = res.corners.reshape((-1, 1, 2)).astype(np.int32)
+                    img = cv2.polylines(img, [pts], isClosed=True, color=(0, 0, 255), thickness=5)
+                    cv2.circle(img, tuple(res.center.astype(np.int32)), 5, (0, 0, 255), -1)
 
-                # print(pose)
+                    curr_y = round(pose[0][0],2)
+                    curr_x = round(pose[0][2], 2)
+                    rot = round(pose[1][2], 2) * 1000 / 3.14
+                    
+                    goal_y = -0.05
+                    goal_x = 0.2
+                    duration = 0.5
+        
+                    vel_x = (curr_x - goal_x) / duration
+                    vel_y = (curr_y - goal_y) / duration
 
-                # (pose[0][0]) # y
-                # (pose[0][2]) # x
-            
+                    ep_chassis.drive_speed(x=vel_x, y=vel_y, z=rot, timeout=duration)
                 
+                elif res.tag_id == 44:
+                    pose = find_pose_from_tag(K, res)
+                    rot, jaco = cv2.Rodrigues(pose[1], pose[1])
+
+                    pts = res.corners.reshape((-1, 1, 2)).astype(np.int32)
+                    img = cv2.polylines(img, [pts], isClosed=True, color=(0, 0, 255), thickness=5)
+                    cv2.circle(img, tuple(res.center.astype(np.int32)), 5, (0, 0, 255), -1)
+
+                    curr_y = round(pose[0][0],2)
+                    curr_x = round(pose[0][2], 2)
+                    rot = round(pose[1][2], 2) * 1000 / 3.14
+                    
+                    goal_y = 0.05
+                    goal_x = 0.2
+                    duration = 0.5
+        
+                    vel_x = (curr_x - goal_x) / duration
+                    vel_y = (curr_y - goal_y) / duration
+
+                    ep_chassis.drive_speed(x=vel_x, y=vel_y, z=rot, timeout=duration)
 
             cv2.imshow("img", img)
             cv2.waitKey(6)
