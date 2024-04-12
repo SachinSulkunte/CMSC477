0700e05d9988df91d2f1c88e88a2d6665c0e3481
AustLin <43714313+AustLin@users.noreply.github.com>
2024-04-12 13:38:51 -0400

    updated quadratic fit model based on measurements

--- Lab2/GetHeightFromBBox.py
+++ Lab2/GetHeightFromBBox.py
@@ -3,22 +3,55 @@ import cv2
 import time
 from robomaster import robot
 from robomaster import camera
-model = YOLO('C:\\Users\\Melin\\OneDrive\\Spring 24\\CMSC477\\Lab 0\\Lab2\\lego.pt')
-# Use vid instead of ep_camera to use your laptop's webcam
-# vid = cv2.VideoCapture(0)
-ep_robot = robot.Robot()
-ep_robot.initialize(conn_type="ap")
-ep_camera = ep_robot.camera
-ep_camera.start_video_stream(display=False, resolution=camera.STREAM_360P)
-while True:
-    # ret, frame = vid.read()
-    frame = ep_camera.read_cv2_image(strategy="newest", timeout=0.5)
-    if frame is not None:
-        if model.predictor:
-            model.predictor.args.verbose = False
-    results = model.predict(source=frame, show=True)
+import numpy as np
 
-    boxes = results[0].boxes.xywh.cpu()
-    for box in boxes:
-        x, y, w, h = box
-        print("Width of Box: {}, Height of Box: {}".format(w, h))
+distanceSignal = []
+
+def getDistanceFromBB():
+    model = YOLO('C:\\Users\\Melin\\OneDrive\\Spring 24\\CMSC477\\Lab 0\\Lab2\\lego.pt')
+    # Use vid instead of ep_camera to use your laptop's webcam
+    # vid = cv2.VideoCapture(0)
+    ep_robot = robot.Robot()
+    ep_robot.initialize(conn_type="ap")
+    ep_camera = ep_robot.camera
+    ep_camera.start_video_stream(display=False, resolution=camera.STREAM_360P)
+    while True:
+        # ret, frame = vid.read()
+        frame = ep_camera.read_cv2_image(strategy="newest", timeout=0.5)
+        if frame is not None:
+            if model.predictor:
+                model.predictor.args.verbose = False
+        results = model.predict(source=frame, show=True)
+
+        boxes = results[0].boxes.xywh.cpu()
+        for box in boxes:
+            x, y, w, h = box
+            print("Width of Box: {}, Height of Box: {}".format(w, h))
+            print("Distance: " + str(distance_from_box_size(w, h)))
+
+import numpy as np
+
+distanceSignal = []
+
+def distance_from_box_size(boxWidth, boxHeight):
+
+    #param
+    a = 80.8847
+    b = -214.7402
+    c = 186.6250
+    expected_aspect_ratio = 7 #h/w
+
+    actual_aspect_ratio = boxHeight / boxWidth
+    correction = expected_aspect_ratio/actual_aspect_ratio
+    actualHeight = boxHeight * correction
+    
+    h_distance = a * actualHeight^2 + b * actualHeight + c 
+    distanceSignal.append(h_distance)
+
+    #rolling median filter
+    if(len(distanceSignal) >= 3):
+        h_distance =  np.median(distanceSignal[-3:])
+
+    return h_distance
+
+getDistanceFromBB()
