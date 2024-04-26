e9a359dde0a098c4ce9a352b8dd6f066e8efd90d
Sachin Sulkunte <sulkunte@gmail.com>
2024-04-26 12:28:07 -0400

    updated

--- Lab2/robot1.py
+++ Lab2/robot1.py
@@ -11,9 +11,117 @@ from numpy.linalg import inv
 from ultralytics import YOLO 
 import math
 
-import detect_blue_line
-
 distanceSignal = []
+
+aa = 300; cc = 50
+def snip_image(img):
+
+    snip = img[(img.shape[0] - aa):(img.shape[0] - cc), (0):(img.shape[1]-100)]
+    return snip
+
+def thres_image(img):
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+
+    # For tracking orange
+    # orangeLower = np.array([0, 103, 220]) 
+    # orangeUpper = np.array([179, 255, 255]) 
+    # orange_mask = cv2.inRange(hsv, orangeLower, orangeUpper)
+    # cv2.imshow("hsv orange mask", orange_mask)
+    # thresholded = cv2.bitwise_and(img,img, mask=orange_mask)
+
+    # For tracking blue
+    blueLower = np.array([96,147,59])
+    blueUpper = np.array([179,255,255])
+    blue_mask = cv2.inRange(hsv, blueLower, blueUpper)
+    #cv2.imshow("hsv",hsv)
+    #cv2.imshow("hsv blue mask", blue_mask)
+    thresholded = cv2.bitwise_and(img,img, mask=blue_mask)
+
+    return thresholded
+
+def blur_img(img):
+    return cv2.GaussianBlur(img,(15,15),0)
+
+def edge_img(img):
+    return cv2.Canny(img, 30, 50)
+
+def line_image(img):
+    return cv2.HoughLines(img, 1, np.pi/180, 20)
+
+def plot_Hough_Lines(img, rho, theta):
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a*rho
+    y0 = b*rho
+    x1 = int(x0 + 1000*(-b))
+    x2 = int(x0 - 1000*(-b))
+    y1 = int(y0 + 1000*a)
+    y2 = int(y0 - 1000*a)
+    cv2.line(img, (x1,y1), (x2,y2), (0, 0, 255), 5)
+
+    a1 = np.cos(theta-theta)
+    b1 = np.sin(theta-theta)
+    x01 = a*rho
+    y01 = b*rho
+    x11 = int(x01 + 1000*(-b1))
+    x21 = int(x01 - 1000*(-b1))
+    y11 = int(y01 + 1000*a1)
+    y21 = int(y01 - 1000*a1)
+
+    cv2.line(img, (x11+272,y11), (x21+272,y21), (0, 255, 0), 5)
+    return img
+
+def getAngle(image):
+    
+    # image = cv2.imread("robot_blorange.jpg")
+
+    # cv2.imshow("Caption",image)
+
+    # snip = snip_image(image)
+    # cv2.imshow("Snip",snip)
+
+    thresholded= thres_image(image)
+    # cv2.imshow("Thresholded Snip",thresholded)
+
+    blurred = blur_img(thresholded)
+    # cv2.imshow("Blurred Image", blurred)
+
+    edged = edge_img(blurred)
+    # cv2.imshow("Edged", edged)
+
+    lines = line_image(edged)
+    print(lines)
+
+    if lines.all() != None:
+    
+        rho = []
+        theta = []
+        for i in range(0, len(lines)):
+            for r, o in lines[i]:
+                rho.append(r)
+                theta.append(o)
+        print(rho)
+        print(theta)
+        print("---")
+        rho_avg = np.mean(rho)
+        theta_avg = np.mean(theta)
+        angle = (180/np.pi)*theta_avg
+
+        final = plot_Hough_Lines(image,rho_avg,theta_avg)
+        cv2.imshow("Final",final)
+
+        print(angle)
+        print("---")
+
+        bottom_percentile = image.shape[0] * 0.9
+        bottom_of_line = max(final[:, 0, 1])
+        is_close = bottom_of_line >= bottom_percentile
+
+        return angle, is_close
+    else:
+        return None, None
 
 def find_pose_from_object(K, x, y, w, h, box):
     w_half_size = w / 2
@@ -85,6 +193,39 @@ def distance_from_box_size(boxWidth, boxHeight):
 
     return h_distance
 
+def detect_blue_line(frame):
+    # Convert the frame to HSV color space
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Define range of blue color in HSV
+    lower_blue = np.array([100, 50, 50])
+    upper_blue = np.array([130, 255, 255])
+
+    # threshold the HSV image to get only blue colors
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    # find contours in the mask
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    if contours:
+        #  largest contour should be blue line
+        blue_contour = max(contours, key=cv2.contourArea)
+        
+        # fit line
+        [vx, vy, x, y] = cv2.fitLine(blue_contour, cv2.DIST_L2, 0, 0.01, 0.01)
+        
+        # calc angle from line direction
+        angle_deg = np.arctan2(vy, vx) * 180 / np.pi
+
+        # check if blue line is in the bottom 10% of pixels of the image
+        bottom_percentile = frame.shape[0] * 0.9
+        bottom_of_line = max(blue_contour[:, 0, 1])
+        is_close = bottom_of_line >= bottom_percentile
+
+        return angle_deg, is_close
+
+    return None, None
+
 if __name__ == '__main__':
     print("start")
     model = YOLO('/Users/sachin/Documents/Coursework/CMSC477/CMSC477/Lab2/best_v2.pt')
@@ -98,7 +239,8 @@ if __name__ == '__main__':
     print("connected")
     ep_gripper = ep_robot.gripper
     ep_arm = ep_robot.robotic_arm
-
+    stage1 = False
+    first = True
     while True:
         try:
             # ret, frame = vid.read()
@@ -113,8 +255,6 @@ if __name__ == '__main__':
             classes = results[0].boxes.cls.tolist()
             names = results[0].names
             confidences = results[0].boxes.conf.tolist()
-
-            stage1 = False
 
             if stage1 is False:
                 # Iterate through the results
@@ -139,44 +279,56 @@ if __name__ == '__main__':
                         # cv2.circle(img, tuple(box.center.astype(np.int32)), 5, (0, 0, 255), -1)
                     
                         # rot = round(pose[1][2], 2) 
-                        
-                        duration = 0.5
-                        goal_x = 10.0 # 10cm away from the lego object
-                        goal_y = 10.0 # 10cm away from the lego object
-                        
-                        dist = float(distance_from_box_size(w, h)) - 0.32
+                        if stage1 is False:
+                            stage1 = True # move on to finding blue line
+                            print(stage1)
+                            duration = 0.5
+                            goal_x = 10.0 # 10cm away from the lego object
+                            goal_y = 10.0 # 10cm away from the lego object
+                            
+                            dist = float(distance_from_box_size(w, h)) - 0.32
 
-                        # vel_x = (distance_from_box_size(w, h) - goal_x)  / duration
-                        # vel_y = (distance_from_box_size(w, h) - goal_y) / duration
-                        # print("vel")
-                        # print(vel_x, vel_y)
-                        # ep_chassis.drive_speed(x=vel_x, y=vel_y, z=0, timeout=duration)
-                        
-                        # open gripper, drive to lego
-                        ep_gripper.open(power=50)
-                        time.sleep(1)
-                        ep_chassis.move(x=dist, y=0, z=0, xy_speed=0.7).wait_for_completed()
-                        time.sleep(1)
-                        ep_gripper.pause()
+                            # vel_x = (distance_from_box_size(w, h) - goal_x)  / duration
+                            # vel_y = (distance_from_box_size(w, h) - goal_y) / duration
+                            # print("vel")
+                            # print(vel_x, vel_y)
+                            # ep_chassis.drive_speed(x=vel_x, y=vel_y, z=0, timeout=duration)
+                            
+                            # open gripper, drive to lego
+                            ep_gripper.open(power=50)
+                            print("opening")
+                            time.sleep(1)
+                            ep_chassis.move(x=dist, y=0, z=0, xy_speed=0.7).wait_for_completed()
+                            time.sleep(1)
+                            ep_gripper.pause()
 
-                        # adjust arm position, close gripper on lego
-                        ep_arm.move(x=goal_x - 1.0, y=goal_y - 1.0).wait_for_completed()
-                        ep_gripper.close(power=50)
-                        time.sleep(1)
-                        ep_gripper.pause()
+                            # adjust arm position, close gripper on lego
+                            ep_arm.move(x=goal_x - 1.0, y=goal_y - 1.0).wait_for_completed()
+                            ep_gripper.close(power=50)
+                            print("closing")
+                            time.sleep(1)
+                            ep_gripper.stop()
 
-                        # lift arm
-                        ep_arm.move(x=0, y=50).wait_for_completed()
-                        stage1 = True # move on to finding blue line
+                            # lift arm
+                            ep_arm.move(x=0, y=50).wait_for_completed()
+                            break
             else:
                 # drive to blue line
-                angle, is_close = detect_blue_line(frame)
-                ep_chassis.move(x=0, y=0, z=-angle, z_speed=20).wait_for_completed() # rotate to blue line
+                print("driving to blue line")
+                angle, is_close = getAngle(frame)
+                print(angle)
+                # angle, is_close = detect_blue_line(frame)
+                ep_chassis.move(x=0, y=0, z=(90 - angle), z_speed=40).wait_for_completed() # rotate to blue line
                 
                 if not is_close: # drive forward if not close yet
-                    ep_chassis.drive_speed(x=0.3, y=0, z=0, timeout=1)
-                    time.sleep(0.5)
+                    ep_chassis.drive_speed(x=0.1, y=0, z=0, timeout=1)
+                    time.sleep(0.4)
                 else:
+                    if first == True:
+                        ep_chassis.move(x=0.2, y=0, z=0, timeout=1)
+                        first = False
+                    print("close")
+                    ep_chassis.drive_speed(x=0, y=0, z=0, timeout=1)
                     # release lego once other robot has arrived
                     time.sleep(5)
                     ep_gripper.open(power=50)
